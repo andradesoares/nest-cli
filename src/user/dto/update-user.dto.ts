8183b910f5cfc2bbe348d8eaee76bb93fc2433da
Daniel Soares <danielandradesoares@hotmail.com>
2022-07-11 16:21:08 -0300

    Body validation

---
+++ src/user/dto/update-user.dto.ts
@@ -0,0 +1,15 @@
+import { InputType } from '@nestjs/graphql';
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+@InputType()
+export class UpdateUserDto {
+  @IsString()
+  @IsNotEmpty({ message: 'Field must be not empty' })
+  @IsOptional()
+  name?: string;
+
+  @IsEmail()
+  @IsNotEmpty({ message: 'Field must be not empty' })
+  @IsOptional()
+  email?: string;
+}
