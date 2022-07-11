38fe01c05afe679b186271868fce0729e055703e
Daniel Soares <danielandradesoares@hotmail.com>
2022-07-11 16:21:36 -0300

    Graphql resolver

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,4 +1,48 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserService } from './user.service';
+import { User } from './user.entity';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Resolver()
-export class UserResolver {}
+export class UserResolver {
+  constructor(private userService: UserService) {}
+
+  @Query(() => [User])
+  async findAllUsers(): Promise<User[]> {
+    const users = await this.userService.findAllUsers();
+
+    return users;
+  }
+
+  @Query(() => User)
+  async findUser(@Args('id') id: string): Promise<User> {
+    const user = await this.userService.findUser(id);
+
+    return user;
+  }
+
+  @Mutation(() => User)
+  async createUser(@Args('data') data: CreateUserDto): Promise<User> {
+    const user = await this.userService.createUser(data);
+
+    return user;
+  }
+
+  @Mutation(() => User)
+  async updateUser(
+    @Args('id') id: string,
+    @Args('data') data: UpdateUserDto,
+  ): Promise<User> {
+    const user = await this.userService.updateUser(id, data);
+
+    return user;
+  }
+
+  @Mutation(() => Boolean)
+  async deleteUser(@Args('id') id: string): Promise<boolean> {
+    const deleted = await this.userService.deleteUser(id);
+
+    return deleted;
+  }
+}
